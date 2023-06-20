ff7aa501ed3fe660f5983d37c1d6901840acdc21
SnJoz <jozef.pavlo@gymsnv.sk>
2023-06-20 13:16:24 +0200

    oprava teploty pre reaktor 2

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -12,10 +12,10 @@ public class Reactor extends AbstractActor {
     private Animation normalAnimation;
 
     public Reactor(){
-        temperature = 0;
-        state = false;
-        damage = 0;
-        normalAnimation = new Animation(
+        this.temperature = 0;
+        this.state = false;
+        this.damage = 0;
+        this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
             80,80,
             0.1F,
@@ -23,5 +23,37 @@ public class Reactor extends AbstractActor {
         );
 
         setAnimation(normalAnimation);
+    }
+
+    public int getTemperature() {
+        return this.temperature;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+    public void increaseTemperature(int increment) {
+        this.temperature = this.temperature + increment;
+
+        // when temperature is above 2000 and below 6000, then damage increases
+        if (this.temperature >= 2000) {
+            if (this.temperature >= 6000) {
+                this.damage = 100;
+            } else {
+                int damage = this.temperature / 40 - 50;
+
+                // update when current damage is bigger than previous
+                if (this.damage < damage) {
+                    this.damage = damage;
+                }
+            //set hot animation
+            if (this.temperature >= 4000) {
+                setAnimation(new Animation("sprites/reactor_hot.png",
+                    80,80,
+                    0.1F,
+                    Animation.PlayMode.LOOP_PINGPONG));
+            }
+            }
+        }
     }
 }
