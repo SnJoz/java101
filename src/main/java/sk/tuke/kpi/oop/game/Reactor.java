d09c06027972af0d074c58876fa233e67c3f70d7
SnJoz <jozef.pavlo@gymsnv.sk>
2023-06-21 10:56:23 +0200

    vyrieseny decrement kod od mireka

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -5,33 +5,33 @@ import sk.tuke.kpi.gamelib.graphics.Animation;
 
 import javax.swing.*;
 
-public class Reactor extends AbstractActor {
-    private int temperature;
+public class Reactor extends AbstractActor {   //toto je trieda
+    private int temperature;  //toto su atributy
     private boolean state;
     private int damage;
     private Animation normalAnimation;
     private Animation hotAnimation;
     private Animation brokenAnimation;
 
-    public Reactor(){
+    public Reactor() {   // toto je metoda - špecialna metoda a tato nič nevracia, služi na inicialkizáciu triedy
         this.temperature = 0;
-        this.state = false;
+        this.state = false;  //členské permenneé patria objektu ktory sme vytvorili
         this.damage = 0;
         this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
-            80,80,
+            80, 80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
         this.hotAnimation = new Animation(
             "sprites/reactor_hot.png",
-            80,80,
+            80, 80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
         this.brokenAnimation = new Animation(
             "sprites/reactor_broken.png",
-            80,80,
+            80, 80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
@@ -41,34 +41,64 @@ public class Reactor extends AbstractActor {
 
     public int getTemperature() {
         return this.temperature;
-    }
+    }  //metody = to su funkcie
+
     public int getDamage() {
         return this.damage;
     }
+
     public void increaseTemperature(int increment) {
+        if(increment < 0){
+            return;
+        }
+
         this.temperature = this.temperature + increment;
 
-        // update animation
+        if (this.damage == 100) {
+            return;
+        }
+
+        updateAnimation();
+
+        // update damage
+        if (this.temperature >= 2000) {
+            if (this.temperature >= 6000) {
+                this.damage = 100;
+            } else {
+                int damage = (this.temperature / 40) - 50;
+                if (this.damage < damage) {
+                    this.damage = damage;
+                }
+            }
+        }
+    }
+
+    public void decreaseTemperature(int decrement) {
+        if(decrement < 0){
+            return;
+        }
+
+        this.temperature = this.temperature - decrement;
+
+        if (this.damage == 100) {
+            return;
+        }
+
+        updateAnimation();
+    }
+
+    private void updateAnimation() {
         // if temperature is >= 6000, then broken show reactor
         if (this.temperature >= 6000) {
             setAnimation(this.brokenAnimation);
-            this.damage = 100;
 
             // if (4000 <= temperature < 6000), then show hot reactor
         } else if (this.temperature >= 4000) {
             setAnimation(this.hotAnimation);
-            int damage = this.temperature / 40 - 50;
 
             // otherwise show normal reactor
         } else {
             setAnimation(this.normalAnimation);
-        }
-        // update damage
-        if (this.temperature > 2000 && this.temperature < 6000) {
-            int damage = this.temperature / 40 - 50;
-            if (this.damage < damage) {
-                this.damage = damage;
-            }
         }
     }
 }
