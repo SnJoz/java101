e9330729ccdebbabf656e4cea6eb0e8f0a54806b
SnJoz <jozef.pavlo@gymsnv.sk>
2023-06-21 09:48:09 +0200

    doplnene animacie skrateny if

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -10,6 +10,8 @@ public class Reactor extends AbstractActor {
     private boolean state;
     private int damage;
     private Animation normalAnimation;
+    private Animation hotAnimation;
+    private Animation brokenAnimation;
 
     public Reactor(){
         this.temperature = 0;
@@ -21,39 +23,43 @@ public class Reactor extends AbstractActor {
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
-
+        this.hotAnimation = new Animation(
+            "sprites/reactor_hot.png",
+            80,80,
+            0.1F,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+        this.brokenAnimation = new Animation(
+            "sprites/reactor_broken.png",
+            80,80,
+            0.1F,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+        // set init reactor animation
         setAnimation(normalAnimation);
     }
 
     public int getTemperature() {
         return this.temperature;
     }
-
     public int getDamage() {
         return this.damage;
     }
     public void increaseTemperature(int increment) {
         this.temperature = this.temperature + increment;
 
-        // when temperature is above 2000 and below 6000, then damage increases
-        if (this.temperature >= 2000) {
-            if (this.temperature >= 6000) {
-                this.damage = 100;
-            } else {
-                int damage = this.temperature / 40 - 50;
+        // update animation
+        // if temperature is >= 6000, then broken show reactor
+        if (this.temperature >= 6000) {
+            setAnimation(this.brokenAnimation);
 
-                // update when current damage is bigger than previous
-                if (this.damage < damage) {
-                    this.damage = damage;
-                }
-            //set hot animation
-            if (this.temperature >= 4000) {
-                setAnimation(new Animation("sprites/reactor_hot.png",
-                    80,80,
-                    0.1F,
-                    Animation.PlayMode.LOOP_PINGPONG));
-            }
-            }
+            // if (4000 <= temperature < 6000), then show hot reactor
+        } else if (this.temperature >= 4000) {
+            setAnimation(this.hotAnimation);
+
+            // otherwise show normal reactor
+        } else {
+            setAnimation(this.normalAnimation);
         }
     }
 }
